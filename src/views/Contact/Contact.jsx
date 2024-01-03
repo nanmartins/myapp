4e9e040bc62f7df592239f6c7cdff585f2ea1d44
Renan Martins <renanmartins989@gmail.com>
2024-01-03 08:49:24 +0000

    basic layout of contact view done, need to improve everything done till now before go to next view

--- src/views/Contact/Contact.jsx
+++ src/views/Contact/Contact.jsx
@@ -1,18 +1,33 @@
 import React from 'react'
 import './Contact.css'
 import SvgContactBust from '../../components/SvgContactBust/SvgContactBust'
+import SocialMediaLinks from '../../components/SocialMediaLinks/SocialMediaLinks'
+import EmailLinkWithIcon from '../../components/EmailLinkWithIcon/EmailLinkWithIcon'
+
 
 const Contact = () => {
   return (
     <div className='contact-container'>
 
       <div className='contact-title-container'>
+        <h1 className='contact-title title-top'>get in</h1>
         <h1 className='contact-title'>Contact</h1>
-        <p className='contact-subtitle'>&#60;Get in touch by social medias /&#62;</p>
+
+        <div className='contact-subtitle'>by
+          <span className='contact-subtitle-highlight'>social media:</span>
+          <SocialMediaLinks />
+        </div>
+
+        <p className='contact-subtitle-email'>or</p>
+
+        <div className='contact-subtitle-email'>by
+          <span className='contact-subtitle-highlight'>email:</span>
+          <EmailLinkWithIcon />
+        </div>
       </div>
 
       <div className='contact-svg-container'>
-        {/* <SvgContactBust /> */}
+        <SvgContactBust />
       </div>
     </div>
   )
