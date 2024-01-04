facc2469cb9fd6aff623d9f616f5a4ae518a0996
Renan Martins <renanmartins989@gmail.com>
2024-01-04 06:53:06 +0000

    need to improve the scroll div large and small screen layout

--- src/views/About/About.jsx
+++ src/views/About/About.jsx
@@ -2,9 +2,54 @@ import React from 'react'
 import './About.css'
 
 const About = () => {
+
   return (
     <div className='about-container'>
-      <h1 className='about-title'>About</h1>
+
+      <div className='about-title-container'>
+        <h1 className='about-title'>About</h1>
+      </div>
+
+      <div className='about-text-container'>
+        <div id="titles">
+          <div id="titlecontent">
+
+            <p className="center">Lorem<br />
+              Lorem ipsum</p>
+
+            <p>It is a period of vendor war.</p>
+
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam debitis consequatur earum quo illo doloremque nesciunt beatae. Blanditiis, magni ex. Numquam aut a necessitatibus minus, recusandae nihil veritatis aliquid exercitationem.</p>
+
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam debitis consequatur earum quo illo doloremque nesciunt beatae. Blanditiis, magni ex. Numquam aut a necessitatibus minus, recusandae nihil veritatis aliquid exercitationem.</p>
+
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam debitis consequatur earum quo illo doloremque nesciunt beatae.</p>
+
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam debitis consequatur earum quo illo doloremque nesciunt beatae. Blanditiis, magni ex. Numquam aut a necessitatibus minus, recusandae nihil veritatis.</p>
+
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam debitis consequatur earum quo illo doloremque nesciunt beatae. Blanditiis, magni ex. Numquam aut a necessitatibus minus, recusandae nihil veritatis aliquid exercitationem.</p>
+
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam debitis consequatur earum quo illo doloremque nesciunt beatae. Blanditiis, magni ex. Numquam aut a necessitatibus minus, recusandae nihil veritatis aliquid exercitationem.</p>
+
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam debitis consequatur earum quo illo doloremque nesciunt beatae. Blanditiis, magni ex. Numquam aut a necessitatibus minus.</p>
+
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam debitis.</p>
+
+            <p className="center">Thanks for your visit.</p>
+
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam debitis consequatur earum quo illo doloremque.</p>
+
+            {/* <p className="center"><a href="#" target='_blank'>mysite.com<br />css3-starwars-scrolling-text/</a></p> */}
+
+            {/* <p>and give me a shout on Twitter <a href="https://twitter.com/craigbuckler">@craigbuckler</a> &ndash; I'd love to see how you use and abuse it!</p> */}
+
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam debitis consequatur earum quo illo doloremque nesciunt beatae. Blanditiis, magni ex. Numquam aut a necessitatibus minus, recusandae nihil veritatis aliquid exercitationem.</p>
+          </div>
+        </div>
+      </div>
+
     </div>
   )
 }
